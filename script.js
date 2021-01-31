525e2faa4076f5145984c13942df6881e8205a71
Vyacheslav <asik2006@mail.ru>
2021-01-31 23:48:52 +0300

    ДЗ 1
    Вывод температуры по фаренгейту, сделано присвоение переменной из другой переменной.

---
+++ script.js
@@ -0,0 +1,12 @@
+var Tc = +prompt('Задайте температуру по целсию = ');
+var Tf = parseInt((9 / 5) * Tc + 32);
+alert('Температара по фаренгейту = ' + Tf);
+
+var Name = "Василий";
+var Admin;
+Admin = Name;
+alert(Admin);
+
+//var a = 1000 + "108";
+//alert(a);
+//Произойдет склеивание числа со строкой
