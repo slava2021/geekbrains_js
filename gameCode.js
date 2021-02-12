8f88db5a54c1b07885cb12e17ee8dc6d42f9d110
Vyacheslav <asik2006@mail.ru>
2021-02-12 17:21:18 +0300

    ДЗ 4 задание 2 выполнено
    Сделана оптимизация цикла while, вынесено в отдельную функцию.

--- gameCode.js
+++ gameCode.js
@@ -6,9 +6,8 @@ var qevent, ok, q, a;
 var answers = [];
 
 q = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
-a = isAnswer(works.a0, qevent, works.a00);
+a = isAnswer(works.a0, q, works.a00);
 whileDo(q, a);
-
 // do {//Выводим первый вопрос
 //     ok = false;
 //     qevent = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
@@ -23,42 +22,57 @@ whileDo(q, a);
 
 switch (qevent) {
     case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
-        do {
-            ok = false;
-            qevent = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
-            if (qevent == -1) {
-                break;
-            }
-            else {
-                ok = isAnswer(works.b0, qevent, works.b00);
-            }
-        } while (!ok);
+
+        q = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+        a = isAnswer(works.b0, q, works.b00);
+        whileDo(q, a);
+
+        // do {
+        //     ok = false;
+        //     qevent = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+        //     if (qevent == -1) {
+        //         break;
+        //     }
+        //     else {
+        //         ok = isAnswer(works.b0, qevent, works.b00);
+        //     }
+        // } while (!ok);
 
         switch (qevent) {
             case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
-                do {
-                    ok = false;
-                    qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                    if (qevent == -1) {
-                        break;
-                    }
-                    else {
-                        ok = isAnswer(works.d0, qevent, works.d00);
-                    }
-                } while (!ok);
+
+                q = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                a = isAnswer(works.d0, q, works.d00);
+                whileDo(q, a);
+
+                // do {
+                //     ok = false;
+                //     qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                //     if (qevent == -1) {
+                //         break;
+                //     }
+                //     else {
+                //         ok = isAnswer(works.d0, qevent, works.d00);
+                //     }
+                // } while (!ok);
 
                 break;
             case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
-                do {
-                    ok = false;
-                    qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                    if (qevent == -1) {
-                        break;
-                    }
-                    else {
-                        ok = isAnswer(works.d0, qevent, works.d00);
-                    }
-                } while (!ok);
+
+                q = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                a = isAnswer(works.d0, q, works.d00);
+                whileDo(q, a);
+
+                // do {
+                //     ok = false;
+                //     qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                //     if (qevent == -1) {
+                //         break;
+                //     }
+                //     else {
+                //         ok = isAnswer(works.d0, qevent, works.d00);
+                //     }
+                // } while (!ok);
 
                 break;
             case -1: // Второе действие
@@ -69,42 +83,57 @@ switch (qevent) {
         break;
 
     case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
-        do {
-            ok = false;
-            qevent = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
-            if (qevent == -1) {
-                break;
-            }
-            else {
-                ok = isAnswer(works.c0, qevent, works.c00);
-            }
-        } while (!ok);
+
+        q = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+        a = isAnswer(works.c0, q, works.c00);
+        whileDo(q, a);
+
+        // do {
+        //     ok = false;
+        //     qevent = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+        //     if (qevent == -1) {
+        //         break;
+        //     }
+        //     else {
+        //         ok = isAnswer(works.c0, qevent, works.c00);
+        //     }
+        // } while (!ok);
 
         switch (qevent) {
             case 1: // Второе действие
-                do {
-                    ok = false;
-                    qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                    if (qevent == -1) {
-                        break;
-                    }
-                    else {
-                        ok = isAnswer(works.d0, qevent, works.d00);
-                    }
-                } while (!ok);
+
+                q = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                a = isAnswer(works.d0, q, works.d00);
+                whileDo(q, a);
+
+                // do {
+                //     ok = false;
+                //     qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                //     if (qevent == -1) {
+                //         break;
+                //     }
+                //     else {
+                //         ok = isAnswer(works.d0, qevent, works.d00);
+                //     }
+                // } while (!ok);
 
                 break;
             case 2: // Второе действие
-                do {
-                    ok = false;
-                    qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
-                    if (qevent == -1) {
-                        break;
-                    }
-                    else {
-                        ok = isAnswer(works.d0, qevent, works.d00);
-                    }
-                } while (!ok);
+
+                q = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                a = isAnswer(works.d0, qevent, works.d00);
+                whileDo(q, a);
+
+                // do {
+                //     ok = false;
+                //     qevent = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                //     if (qevent == -1) {
+                //         break;
+                //     }
+                //     else {
+                //         ok = isAnswer(works.d0, qevent, works.d00);
+                //     }
+                // } while (!ok);
                 break;
             case -1: // Второе действие
                 break;
@@ -126,6 +155,7 @@ do {
         break;
     } else {
         alert(isAnswers(inpval, answers));
+        break;
     }
 } while (!ok);
 //------------------------------------------
@@ -142,6 +172,7 @@ function isAnswer(q, qevent, qopen) {
     return true;
 }
 //------------------------------------------
+// выводим окно и ответ
 function isAnswers(inp, ans) {
     if (inpval < 1 || inpval > 4) {
         alert('Ваше число выходит из допустимого диапозона');
@@ -163,7 +194,8 @@ function isAnswers(inp, ans) {
     }
     return a;
 }
-
+//------------------------------------------
+//основная фонкция обработки действий в игре
 function whileDo(q, a) {
     do {
         ok = false;
